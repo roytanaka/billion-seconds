8ddd051c4bb8e93db0fe4b8b93a4011b57571e9e
Roy Tanaka <tanaka@mac.com>
2020-01-05 17:58:07 -0500

    handelbar template

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import moment from 'moment';
+import template from './template/seconds.hbs';
 import './scss/style.scss';
 
-const date = moment().format();
+const render = function(secondsData) {
+  const main = document.querySelector('#seconds');
+  main.innerHTML = template(secondsData);
+};
 
-console.log(`date value ${date}`);
+const bday = moment([1979, 4, 19]);
+const secondsData = {
+  fromDate: bday.format('MMMM Do YYYY, h:mm a'),
+
+  billionthSecondDay: bday
+    .clone()
+    .add(1000000000, 'seconds')
+    .format('MMMM Do YYYY'),
+};
+console.log('log: secondsData', secondsData);
+setInterval(() => {
+  let now = moment();
+  console.log('log: now', now);
+  let duration = now.diff(bday, 'seconds');
+  console.log('log: duration', duration);
+  secondsData.seconds = duration
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  secondsData.tense = duration < 1000000000 ? 'will be' : 'was';
+  render(secondsData);
+}, 1000);
