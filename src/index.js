2f27ca1f43a67b2e8329fa262da7b0d710f82aae
Roy Tanaka <tanaka@mac.com>
2020-01-04 23:11:55 -0500

    webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import moment from 'moment';
+import './scss/style.scss';
+
+const date = moment().format();
+
+console.log(`date value ${date}`);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    open: 'Google Chrome',
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/*', '!index.html'],
+    }),
+    new MomentLocalesPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
