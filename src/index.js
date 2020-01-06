5767d392ee46a0dbb6d5fb3b6ff28ef1473cf17f
Roy Tanaka <tanaka@mac.com>
2020-01-06 17:28:35 -0500

    modal with animation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import moment from 'moment';
 import secondsTemplate from './template/seconds.hbs';
 import './scss/style.scss';
+import 'animate.css';
 import * as dateBuilder from './date-builder';
 
+// Seconds Template
 const renderSeconds = secondsData => {
   const main = document.querySelector('#seconds');
   main.innerHTML = secondsTemplate(secondsData);
 };
+
+// Modal submit
+const newBirthday = document.querySelector('#getSeconds');
+newBirthday.addEventListener('click', () => {
+  const modal = document.querySelector('.modal');
+  const seconds = document.querySelector('#seconds');
+  const footerDate = document.querySelector('footer .date-selectors');
+  animateCSS('.modal', 'zoomOut', () =>
+    modal.setAttribute('aria-hidden', 'true')
+  );
+
+  animateCSS('#seconds', 'zoomInUp');
+  seconds.setAttribute('aria-hidden', 'false');
+  setTimeout(() => {
+    animateCSS('footer .date-selectors', 'bounceInUp');
+    footerDate.setAttribute('aria-hidden', 'false');
+  }, 800);
+});
 
 // Set date picker
 const years = dateBuilder.years;
@@ -21,7 +41,7 @@ const monthsOptions = months.map(
 );
 document.querySelector('#month').innerHTML += monthsOptions;
 
-// Initial date options
+// Initial date options to 31 days
 const datesOptions = [...Array(31).keys()]
   .map(x => 1 + x)
   .map(date => `<option value="${date}">${date}</option>`);
@@ -43,10 +63,11 @@ const minutesOptions = minutes.map((minute, i) => {
 });
 document.querySelector('#minute').innerHTML += minutesOptions;
 
-setTimeout(() => {
-  bday = moment([1979, 0, 4]);
-  secondsData = dateBuilder.getSecondsData(bday);
-}, 5000);
+// setTimeout(() => {
+//   bday = moment([1979, 0, 4]);
+//   secondsData = dateBuilder.getSecondsData(bday);
+//   animateCSS('#seconds', 'zoomInUp');
+// }, 5000);
 
 let bday = moment([1989, 4, 19]);
 let secondsData = dateBuilder.getSecondsData(bday);
@@ -59,3 +80,17 @@ setInterval(() => {
     .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   renderSeconds(secondsData);
 }, 1000);
+
+function animateCSS(element, animationName, callback) {
+  const node = document.querySelector(element);
+  node.classList.add('animated', animationName);
+
+  function handleAnimationEnd() {
+    node.classList.remove('animated', animationName);
+    node.removeEventListener('animationend', handleAnimationEnd);
+
+    if (typeof callback === 'function') callback();
+  }
+
+  node.addEventListener('animationend', handleAnimationEnd);
+}
