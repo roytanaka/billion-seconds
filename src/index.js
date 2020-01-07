92a0868eb8e330e64b0b1130a7ff5cbfd08e698c
Roy Tanaka <tanaka@mac.com>
2020-01-07 10:00:46 -0500

    animated modal, date picker works

--- src/index.js
+++ src/index.js
@@ -10,12 +10,88 @@ const renderSeconds = secondsData => {
   main.innerHTML = secondsTemplate(secondsData);
 };
 
-// Modal submit
+// Set date picker
+const year = document.querySelector('#year');
+const modalYear = document.querySelector('#modalYear');
+const yearsOptions = dateBuilder.years.map(
+  year => `<option value="${year}">${year}</option>`
+);
+year.innerHTML = yearsOptions;
+modalYear.innerHTML = yearsOptions;
+
+const month = document.querySelector('#month');
+const modalMonth = document.querySelector('#modalMonth');
+const months = dateBuilder.months;
+const monthsOptions = months.map(
+  (month, i) => `<option value="${i}">${month}</option>`
+);
+month.innerHTML = monthsOptions;
+modalMonth.innerHTML = monthsOptions;
+
+const date = document.querySelector('#date');
+const modalDate = document.querySelector('#modalDate');
+const datesOptions = [...Array(31).keys()]
+  .map(x => 1 + x)
+  .map(date => `<option value="${date}">${date}</option>`);
+date.innerHTML = datesOptions;
+modalDate.innerHTML = datesOptions;
+
+const hour = document.querySelector('#hour');
+const modalHour = document.querySelector('#modalHour');
+const hours = dateBuilder.hours;
+const hoursOptions = hours.map((hour, i) => {
+  return `<option value="${i}">${hour
+    .toString()
+    .replace(/(^\d$)/, '0$1')}</option>`;
+});
+hour.innerHTML = hoursOptions;
+modalHour.innerHTML = hoursOptions;
+
+const minute = document.querySelector('#minute');
+const modalMinute = document.querySelector('#modalMinute');
+const minutes = dateBuilder.minutes;
+const minutesOptions = minutes.map((minute, i) => {
+  return `<option value="${i}">${minute
+    .toString()
+    .replace(/(^\d$)/, '0$1')}</option>`;
+});
+minute.innerHTML = minutesOptions;
+modalMinute.innerHTML = minutesOptions;
+
+const modal = document.querySelector('.modal');
+const selectors = modal.querySelectorAll('select');
+for (const select of selectors) {
+  select.addEventListener('change', () => {
+    updateDate(
+      modalYear.value,
+      modalMonth.value,
+      modalDate.value,
+      modalHour.value,
+      modalMinute.value
+    );
+  });
+}
+
+const footerSelectors = document.querySelectorAll('footer select');
+for (const select of footerSelectors) {
+  select.addEventListener('change', () => {
+    updateDate(year.value, month.value, date.value, hour.value, minute.value);
+  });
+}
+
 const newBirthday = document.querySelector('#getSeconds');
+
+// Modal submit
 newBirthday.addEventListener('click', () => {
-  const modal = document.querySelector('.modal');
+  year.value = modalYear.value;
+  month.value = modalMonth.value;
+  date.value = modalDate.value;
+  hour.value = modalHour.value;
+  minute.value = modalMinute.value;
+
   const seconds = document.querySelector('#seconds');
   const footerDate = document.querySelector('footer .date-selectors');
+
   animateCSS('.modal', 'zoomOut', () =>
     modal.setAttribute('aria-hidden', 'true')
   );
@@ -28,49 +104,21 @@ newBirthday.addEventListener('click', () => {
   }, 800);
 });
 
-// Set date picker
-const years = dateBuilder.years;
-const yearsOptions = years.map(
-  year => `<option value="${year}">${year}</option>`
-);
-document.querySelector('#year').innerHTML += yearsOptions;
-
-const months = dateBuilder.months;
-const monthsOptions = months.map(
-  (month, i) => `<option value="${i}">${month}</option>`
-);
-document.querySelector('#month').innerHTML += monthsOptions;
-
-// Initial date options to 31 days
-const datesOptions = [...Array(31).keys()]
-  .map(x => 1 + x)
-  .map(date => `<option value="${date}">${date}</option>`);
-document.querySelector('#date').innerHTML += datesOptions;
-
-const hours = dateBuilder.hours;
-const hoursOptions = hours.map((hour, i) => {
-  return `<option value="${i}">${hour
-    .toString()
-    .replace(/(^\d$)/, '0$1')}</option>`;
-});
-document.querySelector('#hour').innerHTML += hoursOptions;
-
-const minutes = dateBuilder.minutes;
-const minutesOptions = minutes.map((minute, i) => {
-  return `<option value="${i}">${minute
-    .toString()
-    .replace(/(^\d$)/, '0$1')}</option>`;
-});
-document.querySelector('#minute').innerHTML += minutesOptions;
-
 // setTimeout(() => {
 //   bday = moment([1979, 0, 4]);
 //   secondsData = dateBuilder.getSecondsData(bday);
 //   animateCSS('#seconds', 'zoomInUp');
 // }, 5000);
 
-let bday = moment([1989, 4, 19]);
-let secondsData = dateBuilder.getSecondsData(bday);
+let bday;
+let secondsData;
+
+function updateDate(year, month, date, hour, minute) {
+  bday = moment([year, month, date, hour, minute]);
+  secondsData = dateBuilder.getSecondsData(bday);
+}
+
+updateDate(2020, 0, 1, 0, 0);
 
 setInterval(() => {
   let interval = moment();
@@ -79,7 +127,7 @@ setInterval(() => {
     .toString()
     .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   renderSeconds(secondsData);
-}, 1000);
+}, 100);
 
 function animateCSS(element, animationName, callback) {
   const node = document.querySelector(element);
